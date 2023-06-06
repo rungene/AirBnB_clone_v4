c410fb4c3eed8d8591d7c34710807bdb42eecb8d
rungene <rungenesamuel@gmail.com>
2023-06-06 03:30:15 -0700

    Write a JavaScript script (static/scripts/4-hbnb.js): Based on 3-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,11 +1,12 @@
-// Execute the script only when DOM is loaded
-$(document).ready(function () {
-  // send a post request to places_search endpoint
+function searchPlaces () {
+// send a post request to places_search endpoint
   $.ajax({
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     contentType: 'application/json',
-    data: JSON.stringify({}),
+    data: JSON.stringify({
+      amenities: Object.keys(selectedAmenities)
+      }),
     success: function (response) {
       // Clear existing places
       $('.places').empty();
@@ -31,4 +32,34 @@ $(document).ready(function () {
       }
     }
   });
+}  
+// Execute the script only when DOM is loaded
+$(document).ready(function () {
+  // Variable to store the selected Amenity IDS
+  const selectedAmenities = {};
+
+  // Listen for changes on each input checkbox tag
+  $('input["checkbox"]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+
+    // Check if checkbox is checked
+    if ($(this).is(':checked')) {
+      // Store the Amenity ID if the variable 
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      // Remove the Amenity ID from the variable
+      delete selectedAmenities[amenityId];
+    }
+    // Update the h4 tag inside the div Amenities with the list of checked Amenities
+    const amenitiesList = Object.values(selectedAmenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+  // Listen for click event on the button tag
+  $('button').click(function () {
+    // Make a Post request to search_place with the list of checked amenities
+    searchPlaces();
+  });
+  // Initialise the place search when DOM is loaded
+  searchPlaces();
 });
